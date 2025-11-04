LineageOS/android_hardware_oplus

kernel-headers: Add PANEL_IOCTL_{GET,SET}_PWM_PULSE
Change-Id: Ia81c01fd2eb707e06d1cf40f90585f2da01fe934
Signed-off-by: inferno0230 <mail@inferno0230.in>

kernel-headers/include/oplus/oplus_display_panel.h
@@ -30,5 +30,7 @@
 #define PANEL_IOCTL_SET_FP_PRESS _IOW(OPLUS_PANEL_IOCTL_BASE, 0x29, unsigned int)
 #define PANEL_IOCTL_SET_CABC_STATUS _IOW(OPLUS_PANEL_IOCTL_BASE, 0x59, unsigned int)
 #define PANEL_IOCTL_GET_CABC_STATUS _IOWR(OPLUS_PANEL_IOCTL_BASE, 0x5A, unsigned int)
+#define PANEL_IOCTL_SET_PWM_PULSE _IOW(OPLUS_PANEL_IOCTL_BASE, 0x72, unsigned int)
+#define PANEL_IOCTL_GET_PWM_PULSE _IOW(OPLUS_PANEL_IOCTL_BASE, 0x73, unsigned int)
 #define PANEL_IOCTL_SET_PWM_TURBO _IOW(OPLUS_PANEL_IOCTL_BASE, 0x66, unsigned int)
 #define PANEL_IOCTL_GET_PWM_TURBO _IOWR(OPLUS_PANEL_IOCTL_BASE, 0x67, unsigned int)

lemonadep-crd/pos_hardware_oplus

kernel-headers/include/oplus/oplus_display_panel.h
@@ -32,3 +32,5 @@
 #define PANEL_IOCTL_GET_CABC_STATUS _IOWR(OPLUS_PANEL_IOCTL_BASE, 0x5A, unsigned int)
 #define PANEL_IOCTL_SET_PWM_TURBO _IOW(OPLUS_PANEL_IOCTL_BASE, 0x66, unsigned int)
 #define PANEL_IOCTL_GET_PWM_TURBO _IOWR(OPLUS_PANEL_IOCTL_BASE, 0x67, unsigned int)
+#define PANEL_IOCTL_SET_PWM_PULSE _IOW(OPLUS_PANEL_IOCTL_BASE, 0x72, unsigned int)
+#define PANEL_IOCTL_GET_PWM_PULSE _IOWR(OPLUS_PANEL_IOCTL_BASE, 0x73, unsigned int)